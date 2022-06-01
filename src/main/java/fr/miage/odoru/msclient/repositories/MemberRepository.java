dbc465e46a7dd55ce8ae5d19cfd9e9fb0963d1ac
Alexis V <alexis.vivier@toulouse.miage.fr>
2022-06-01 15:51:20 +0200

    Ajout de la recherche par utilisateur

--- src/main/java/fr/miage/odoru/msclient/repositories/MemberRepository.java
+++ src/main/java/fr/miage/odoru/msclient/repositories/MemberRepository.java
@@ -4,6 +4,9 @@ import fr.miage.odoru.msclient.entities.Member;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MemberRepository extends CrudRepository<Member, Long> {
+    List<Member> findByUsername(String username);
 }
