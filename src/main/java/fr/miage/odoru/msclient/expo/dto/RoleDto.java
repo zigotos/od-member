7b6855324f40c8c7da7a61212747c1ec7730b064
DamienLinux <damien.avetta@gmail.com>
2022-06-24 09:54:34 +0200

    RoleDto Member

---
+++ src/main/java/fr/miage/odoru/msclient/expo/dto/RoleDto.java
@@ -0,0 +1,18 @@
+package fr.miage.odoru.msclient.expo.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import fr.miage.odoru.msclient.entities.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class RoleDto implements Serializable {
+    private String username;
+    private List<Member.Roles> roles;
+}
